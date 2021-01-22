76d680fc0067427a523a7ff8ca0554feb2bf7369
Jamesdennis <Jamesdennis869@gmail.com>
2021-01-23 01:11:11 +0300

    whatsapp_setting_clone: update

--- app/src/main/java/com/example/whatsapp/account.java
+++ app/src/main/java/com/example/whatsapp/account.java
@@ -4,12 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
+
 public class account extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.settings, new accounts())
+                .commit();
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    public static class accounts extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.root_accounts, rootKey);
+        }
     }
 }
--- app/src/main/java/com/example/whatsapp/notification.java
+++ app/src/main/java/com/example/whatsapp/notification.java
@@ -4,12 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.preference.PreferenceFragmentCompat;
+
 public class notification extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.settings, new notifications())
+                .commit();
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+    public static class notifications extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.root_notifications, rootKey);
+        }
     }
 }
